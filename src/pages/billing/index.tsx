c0133907e8178af545c48bfd8916ebcb5e3b6a69
Rafael Renck <rafaelrenck@gmail.com>
2021-11-19 15:05:53 -0300

    chore: basic design pagination component

--- src/pages/billing/index.tsx
+++ src/pages/billing/index.tsx
@@ -7,6 +7,7 @@ import { BiFile, BiSpreadsheet, BiBone, BiVial, BiTime } from "react-icons/bi";
 
 import { Input } from "../../components/Form/Input";
 import { Select } from "../../components/Form/Select";
+import { Pagination } from "../../components/Pagination";
 
 export function Attach() {
   const [filter, setFilter] = useState({ month: new Date().toISOString().substring(0, 7), insurance: "13" , amb: true, ext: false, int: false});
@@ -15,18 +16,30 @@ export function Attach() {
   const [loading, setLoading] = useState(true);
 
   function handleDateChange(month: string) {
+    setAppointments([]);
+    setLoading(true);
+
     setFilter({ ...filter, month: month });
   }
 
   function handleTypeAmbChange() {
+    setAppointments([]);
+    setLoading(true);
+
     setFilter({ ...filter, amb: !filter.amb });
   }
 
   function handleTypeExtChange() {
+    setAppointments([]);
+    setLoading(true);
+
     setFilter({ ...filter, ext: !filter.ext });
   }
 
   function handleTypeIntChange() {
+    setAppointments([]);
+    setLoading(true);
+
     setFilter({ ...filter, int: !filter.int });
   }
 
@@ -62,146 +75,151 @@ export function Attach() {
     <Box
       as="section"
       flex="1"
-      borderRadius="18px"
-      bg="gray.800"
-      p="2rem"
     >
-      <Flex mb="4rem" alignItems="center" justifyContent="space-between">
-        <Heading size="lg" textTransform="uppercase">Anexar Documentos</Heading>
-        <IconButton
-          aria-label="Ajuda"
-          icon={<FaQuestion />}
-          colorScheme="transparent"
-          fontSize="1.2rem"
-          color="gray.600"
-          isRound
-        />
-      </Flex>
-      <Flex mb="2rem">
-        <HStack spacing="2rem" bg="gray.800">
-          <Input
-            type="month"
-            name="scope"
-            label="Competência"
-            value={filter.month}
-            onChange={(e) => handleDateChange(e.target.value)}
+      <Box
+        borderRadius="18px"
+        bg="gray.800"
+        p="2rem"
+      >
+        <Flex mb="4rem" alignItems="center" justifyContent="space-between">
+          <Heading size="lg" textTransform="uppercase">Anexar Documentos</Heading>
+          <IconButton
+            aria-label="Ajuda"
+            icon={<FaQuestion />}
+            colorScheme="transparent"
+            fontSize="1.2rem"
+            color="gray.600"
+            isRound
           />
-          <Checkbox colorScheme="primary" defaultIsChecked={filter.amb} onChange={(e) => handleTypeAmbChange()}>AMB</Checkbox>
-          <Checkbox colorScheme="primary" defaultIsChecked={filter.ext} onChange={(e) => handleTypeExtChange()}>EXT</Checkbox>
-          <Checkbox colorScheme="primary" defaultIsChecked={filter.int} onChange={(e) => handleTypeIntChange()}>INT</Checkbox>
-          <Select name="health_insurance" label="Convênio" onChange={(e) => handleInsuranceChange(e.target.value)}>
-            {insurances.map(insurance => (
-              <option key={insurance.id} value={insurance.id} selected={insurance.id == filter.insurance ? true : false}>{insurance.name}</option>
-            ))}
-          </Select>
-        </HStack>
-      </Flex>
-      { loading ? (
-        <Flex justify="center" mt="5rem">
-          <Spinner size="xl" />
         </Flex>
-      ) : (
-        <Table colorScheme="whiteAlpha">
-          <Thead>
-            <Tr>
-              <Th textAlign="center" pl="1rem" pr="2rem">Data</Th>
-              <Th w="100%">Paciente</Th>
-              <Th textAlign="center" pr="2rem">Tipo</Th>
-              <Th textAlign="center" px="0" minW="4rem">Conta</Th>
-              <Th textAlign="center" px="0" minW="4rem">TISS</Th>
-              <Th textAlign="center" px="0" minW="4rem">Scan</Th>
-              <Th textAlign="center" px="0" minW="4rem">RX</Th>
-              <Th textAlign="center" px="0" minW="4rem">Lab</Th>
-              <Th textAlign="center" pl="2rem" pr="1rem">Upload</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {appointments.map(appointment => (
-              <Tr key={appointment.id_fia}>
-                <Td textAlign="center" whiteSpace="nowrap" pl="1rem" pr="2rem">
-                  <Stack alignItems="center" spacing="0.2rem" fontSize="0.8rem">
-                    <Flex alignItems="center">
-                      <Text>{format(parseISO(appointment.date), 'dd/MM/yy')}</Text>
-                    </Flex>
-                    <Flex alignItems="center" color="gray.600">
-                      <Icon as={BiTime} mr="0.5rem" />
-                      <Text>{appointment.hour.substring(0, 5)}</Text>
-                    </Flex>
-                  </Stack>
-                </Td>
-                <Td>
-                  <Stack spacing="0.2rem">
-                    <Text fontWeight="semibold">{appointment.patient}</Text>
-                    <HStack spacing="2rem" color="gray.600" fontSize="0.8rem">
-                      <Text>
-                        Registro:
-                        <Text as="span" ml="0.5rem" fontWeight="bold">{appointment.id_patient}</Text>
-                      </Text>
-                    </HStack>
-                  </Stack>
-                </Td>
-                <Td textAlign="center" pr="2rem">
-                  <Text color="gray.600">{appointment.type}</Text>
-                </Td>
-                <Td textAlign="center" px="0">
-                  <IconButton
-                    aria-label="Conta Corrente"
-                    icon={<BiFile />}
-                    colorScheme="black"
-                    fontSize="1.2rem"
-                    isRound
-                  />
-                </Td>
-                <Td textAlign="center" px="0">
-                  <IconButton
-                    aria-label="Enviar arquivo"
-                    icon={<BiSpreadsheet />}
-                    colorScheme="black"
-                    fontSize="1.2rem"
-                    isRound
-                  />
-                </Td>
-                <Td textAlign="center" px="0">
-                  <IconButton
-                    aria-label="Enviar arquivo"
-                    icon={<FaSignature />}
-                    colorScheme="black"
-                    fontSize="1.2rem"
-                    isRound
-                  />
-                </Td>
-                <Td textAlign="center" px="0">
-                  <IconButton
-                    aria-label="Enviar arquivo"
-                    icon={<BiBone />}
-                    colorScheme="black"
-                    fontSize="1.2rem"
-                    isRound
-                  />
-                </Td>
-                <Td textAlign="center" px="0">
-                  <IconButton
-                    aria-label="Enviar arquivo"
-                    icon={<BiVial />}
-                    colorScheme="black"
-                    fontSize="1.2rem"
-                    isRound
-                  />
-                </Td>
-                <Td textAlign="center" pl="2rem" pr="1rem">
-                  <IconButton
-                    aria-label="Enviar arquivo"
-                    icon={<AiOutlinePaperClip />}
-                    colorScheme="primary"
-                    fontSize="1.2rem"
-                    isRound
-                  />
-                </Td>
+        <Flex mb="2rem">
+          <HStack spacing="2rem" bg="gray.800">
+            <Input
+              type="month"
+              name="scope"
+              label="Competência"
+              value={filter.month}
+              onChange={(e) => handleDateChange(e.target.value)}
+            />
+            <Checkbox colorScheme="primary" defaultIsChecked={filter.amb} onChange={(e) => handleTypeAmbChange()}>AMB</Checkbox>
+            <Checkbox colorScheme="primary" defaultIsChecked={filter.ext} onChange={(e) => handleTypeExtChange()}>EXT</Checkbox>
+            <Checkbox colorScheme="primary" defaultIsChecked={filter.int} onChange={(e) => handleTypeIntChange()}>INT</Checkbox>
+            <Select name="health_insurance" label="Convênio" onChange={(e) => handleInsuranceChange(e.target.value)}>
+              {insurances.map(insurance => (
+                <option key={insurance.id} value={insurance.id} selected={insurance.id == filter.insurance ? true : false}>{insurance.name}</option>
+              ))}
+            </Select>
+          </HStack>
+        </Flex>
+        { loading ? (
+          <Flex justify="center" mt="5rem">
+            <Spinner size="xl" />
+          </Flex>
+        ) : (
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th textAlign="center" pl="1rem" pr="2rem">Data</Th>
+                <Th w="100%">Paciente</Th>
+                <Th textAlign="center" pr="2rem">Tipo</Th>
+                <Th textAlign="center" px="0" minW="4rem">Conta</Th>
+                <Th textAlign="center" px="0" minW="4rem">TISS</Th>
+                <Th textAlign="center" px="0" minW="4rem">Scan</Th>
+                <Th textAlign="center" px="0" minW="4rem">RX</Th>
+                <Th textAlign="center" px="0" minW="4rem">Lab</Th>
+                <Th textAlign="center" pl="2rem" pr="1rem">Upload</Th>
               </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      )}
+            </Thead>
+            <Tbody>
+              {appointments.map(appointment => (
+                <Tr key={appointment.id_fia}>
+                  <Td textAlign="center" whiteSpace="nowrap" pl="1rem" pr="2rem">
+                    <Stack alignItems="center" spacing="0.2rem" fontSize="0.8rem">
+                      <Flex alignItems="center">
+                        <Text>{format(parseISO(appointment.date), 'dd/MM/yy')}</Text>
+                      </Flex>
+                      <Flex alignItems="center" color="gray.600">
+                        <Icon as={BiTime} mr="0.5rem" />
+                        <Text>{appointment.hour.substring(0, 5)}</Text>
+                      </Flex>
+                    </Stack>
+                  </Td>
+                  <Td>
+                    <Stack spacing="0.2rem">
+                      <Text fontWeight="semibold">{appointment.patient}</Text>
+                      <HStack spacing="2rem" color="gray.600" fontSize="0.8rem">
+                        <Text>
+                          Registro:
+                          <Text as="span" ml="0.5rem" fontWeight="bold">{appointment.id_patient}</Text>
+                        </Text>
+                      </HStack>
+                    </Stack>
+                  </Td>
+                  <Td textAlign="center" pr="2rem">
+                    <Text color="gray.600">{appointment.type}</Text>
+                  </Td>
+                  <Td textAlign="center" px="0">
+                    <IconButton
+                      aria-label="Conta Corrente"
+                      icon={<BiFile />}
+                      colorScheme="black"
+                      fontSize="1.2rem"
+                      isRound
+                    />
+                  </Td>
+                  <Td textAlign="center" px="0">
+                    <IconButton
+                      aria-label="Enviar arquivo"
+                      icon={<BiSpreadsheet />}
+                      colorScheme="black"
+                      fontSize="1.2rem"
+                      isRound
+                    />
+                  </Td>
+                  <Td textAlign="center" px="0">
+                    <IconButton
+                      aria-label="Enviar arquivo"
+                      icon={<FaSignature />}
+                      colorScheme="black"
+                      fontSize="1.2rem"
+                      isRound
+                    />
+                  </Td>
+                  <Td textAlign="center" px="0">
+                    <IconButton
+                      aria-label="Enviar arquivo"
+                      icon={<BiBone />}
+                      colorScheme="black"
+                      fontSize="1.2rem"
+                      isRound
+                    />
+                  </Td>
+                  <Td textAlign="center" px="0">
+                    <IconButton
+                      aria-label="Enviar arquivo"
+                      icon={<BiVial />}
+                      colorScheme="black"
+                      fontSize="1.2rem"
+                      isRound
+                    />
+                  </Td>
+                  <Td textAlign="center" pl="2rem" pr="1rem">
+                    <IconButton
+                      aria-label="Enviar arquivo"
+                      icon={<AiOutlinePaperClip />}
+                      colorScheme="primary"
+                      fontSize="1.2rem"
+                      isRound
+                    />
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        )}
+        <Pagination />
+      </Box>
+
     </Box>
   );
 }
